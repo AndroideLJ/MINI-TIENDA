74e58d58975b2f99be530a0731e1a9c40ec2ee42
Androidejl <inglopezjulio@gmail.com>
2023-01-09 13:35:36 -0500

    Cracion del formulario de restablecer contraseña, y funcion del boton reenviar correo

--- Pass/Restaurar-pass.js
+++ Pass/Restaurar-pass.js
@@ -1,53 +1,73 @@
-function onDoPass(){
-    const email = document.getElementById('inputEmail').value;
-    const ident = document.getElementById('inputIdent').value;
+function onDoPass() {
+    const email = document.getElementById('inputEmail');
+    const ident = document.getElementById('inputIdent');
 
-    if(email == '' && ident == ''){
-        alert("Email e identificacion requeridos");
+    if (email.value == '' && ident.value == '') {
+        alert("Usuario y Contraseña requeridos");
         return;
     }
 
-    if(ident == ''){
-        alert("Por favor, ingresa su identificacion")
-        document.getElementById('inputEmail').value = "";
-        return;
-    }
-
-    if(email == ''){
-        alert("Por favor, ingresa tu email")
-        document.getElementById('inputIdent').value = "";
+    const fieldInvalids = validateInput(email, 'Email requerido') || validateInput(ident, 'Identificacion requerido')
+    if (fieldInvalids) {
         return;
     }
 
     alert('Se ha enviado un correo de restablecimiento')
-    formPass.reset();
 
-
+    startCountDown();
+    disableResendMail();
 }
 
-function onDoRenviar(){
+function onDoRenviar() {
     const email = document.getElementById('inputEmail').value;
     const ident = document.getElementById('inputIdent').value;
 
-    if(email == '' && ident == ''){
-        alert("Por favor, llene los campos");
-        return;
-    }
-    
-
-    if(ident == ''){
-        alert("Por favor, ingresa su identificacion")
-        document.getElementById('inputEmail').value = "";
+    if (email.value == '' && ident.value == '') {
+        alert("Usuario y Contraseña requeridos");
         return;
     }
 
-    if(email == ''){
-        alert("Por favor, ingresa tu email")
-        document.getElementById('inputIdent').value = "";
+    const fieldInvalids = validateInput(email, 'Email requerido') || validateInput(ident, 'Identificacion requerido')
+    if (fieldInvalids) {
         return;
     }
 
     alert('Se reenvio nuevamente el mensaje')
-    formPass.reset();
 
+    formPass.reset();
+    disableResendMail();
+    startCountDown();
+
+}
+
+function validateInput(element, message) {
+    const isEmpty = element.value === '';
+    if (isEmpty) {
+        alert(message);
+        formPass.reset();
+    }
+    return isEmpty;
+}
+
+function disableResendMail() {
+    document.getElementById('reenviarButton').classList.add('resend-disable')
+    setTimeout(function () {
+        document.getElementById('reenviarButton').classList.remove('resend-disable')
+    }, 5000);
+}
+
+function startCountDown(){
+    var second = 5;
+    document.getElementById('counter').innerHTML='00:0'+second;
+    var interval = setInterval(function() {
+        
+        if( second === 0){
+            clearInterval(interval);
+            document.getElementById('counter').innerHTML='';
+        } else {
+            document.getElementById('counter').innerHTML='00:0'+second;
+            second--;
+        }
+        
+    }, 1000);
 }
